f362a47533098b055865df2b1d5a527e94199343
Nick Galbreath <nickg@client9.com>
2014-04-28 00:48:48 +0900

    fix path;

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -fs "`pwd`/dot.emacs.el" ~/.emacs
 mkdir -p ~/.emacs.d/site-lisp
 ln -fs "`pwd`/dot.emacs.d/site-lisp" ~/.emacs.d/site-lisp
 
+mkdir -p dot.emacs.d/site-lisp
 cd dot.emacs.d/site-lisp/
 if [ ! -d go ]; then
   git clone https://github.com/dominikh/go-mode.el go
