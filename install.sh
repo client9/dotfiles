90a5c9cfd2ff948e86f4258b4cf6116fbfc6a904
Nick Galbreath <nickg@client9.com>
2014-07-18 03:26:58 +0000

    fix

--- install.sh
+++ install.sh
@@ -3,11 +3,8 @@
 ln -fs "`pwd`/dot.emacs.el" ~/.emacs
 
 rm -rf ~/.emacs.d
-mkdir -p ~/.emacs.d
-ln -fs "`pwd`/dot.emacs.d/site-lisp" ~/.emacs.d/
-
-mkdir -p dot.emacs.d/site-lisp
-cd dot.emacs.d/site-lisp/
+mkdir -p ~/.emacs.d/site-lisp
+cd ~/.emacs.d/site-lisp/
 if [ ! -d go ]; then
   git clone https://github.com/dominikh/go-mode.el go
 else
