0591ed35dc981b8061306904d85d63ef7cd7e83d
nickg <nickg@signalsciences.com>
2018-11-20 22:29:42 -0800

    add ssh config

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 cp -f dot.gitconfig ~/.gitconfig
 cp -f dot.bash_profile ~/.bash_profile
 mkdir -p ~/.ssh
-cp -f id_rsa.pub ~/.ssh
+cp -f ssh-config ~/.ssh/.config
+cp -f id_rsa.pub ~/.ssh/id_rsa.pub
 
