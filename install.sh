1ee54422101707cbdb4671436c304a6d509794c1
nickg <nickg@client9.com>
2018-11-20 23:23:37 -0800

    start of vim

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -x
 cp -f dot.gitconfig ~/.gitconfig
 cp -f dot.bash_profile ~/.bash_profile
+
+#
+# SSH
+#
 mkdir -p ~/.ssh
 cp -f ssh-config ~/.ssh/.config
 cp -f id_rsa.pub ~/.ssh/id_rsa.pub
+
+#
+# VIM
+#
+cp dot.vimrc ~/.vimrc
 
 if [ -d  ~/.vim/pack/plugins/start/vim-go ]; then
   cd ~/.vim/pack/plugins/start/vim-go && git update
